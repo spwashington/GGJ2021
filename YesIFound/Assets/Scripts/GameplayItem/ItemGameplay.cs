ddbda2548ad920a433fa895c452ed04483d80167
Diego Jesus Almeida <diego.jacare@terra.com.br>
2021-01-30 20:11:54 -0300

    Prefabs

--- YesIFound/Assets/Scripts/GameplayItem/ItemGameplay.cs
+++ YesIFound/Assets/Scripts/GameplayItem/ItemGameplay.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class ItemGameplay : MonoBehaviour
 {
-    [SerializeField] SpriteRenderer m_light;
+    [SerializeField] SpriteRenderer m_light, m_objSprite;
+    private string m_ItemName, m_Color;
 
     private void Awake()
     {
         Unhighlight();
+    }
+
+    public void Atributes(string _name, string _color, Sprite _sprite)
+    {
+        m_ItemName = _name;
+        m_Color = _color;
+        m_objSprite.sprite = _sprite;
     }
 
     public void Highlight()
