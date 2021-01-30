a01de761abd8c7373a332ab8617ed1f563f00820
Diego Jesus Almeida <diego.jacare@terra.com.br>
2021-01-30 13:32:23 -0300

    [CRATE] Make chracter movement

---
+++ YesIFound/Assets/Scripts/Player/Scripts/PlayerAtribsSO.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Player Atribs", menuName = "Scriptables/Player", order = 1)]
+public class PlayerAtribsSO : ScriptableObject
+{
+    [Header("Player Inputs")]
+    public KeyCode GetAnDropElementsButton;
+    public KeyCode DashButton;
+
+    [Header("Player Speed")]
+    public float WalkSpeed;
+
+    public float DashSpeed;
+    public float DashDuration;
+    public float DashCooldown;
+}
