38ad62be298c2c7c7c1033a03d124a766570d04c
zBlurryface <42378342+zBlurryface@users.noreply.github.com>
2021-01-30 18:28:28 -0300

    MainMenu animation

--- YesIFound/Assets/Scripts/MainMenu/MenuController.cs
+++ YesIFound/Assets/Scripts/MainMenu/MenuController.cs
@@ -4,16 +4,34 @@ using UnityEngine;
 
 public class MenuController : MonoBehaviour
 {
-    
+    public GameObject cam; 
+    private Animator camAnim;
+
+    private void Awake()
+    {
+        camAnim = cam.GetComponent<Animator>();
+    }
+
     public void PlayGame()
     {
-        Debug.Log("Play");
+        
+        StartCoroutine(CamTrigger());
     }
 
     public void QuitGame()
     {
         Debug.Log("Exit");
         Application.Quit();
+    }
+
+    private IEnumerator CamTrigger()
+    {
+        yield return new WaitForSeconds(4f);
+        camAnim.SetBool("isStartCamera", true);
+        Debug.Log("Play");
+
+        yield return null;
+        camAnim.SetBool("isStartCamera", false);
     }
 
 }
