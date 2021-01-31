2bf079e74321358fa4a08616123c8e7b44a9f650
zBlurryface <42378342+zBlurryface@users.noreply.github.com>
2021-01-31 01:02:38 -0300

    ChangeCameras

--- YesIFound/Assets/Scripts/MainMenu/CamAnimEnd.cs
+++ YesIFound/Assets/Scripts/MainMenu/CamAnimEnd.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class CamAnimEnd : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public bool endAnim;
+
+
+
+    public void FinalAnim()
     {
-        
+        endAnim = true;
     }
 }
--- YesIFound/Assets/Scripts/MainMenu/MenuController.cs
+++ YesIFound/Assets/Scripts/MainMenu/MenuController.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class MenuController : MonoBehaviour
 {
-    public GameObject cam; 
+    public GameObject cam;
+    public GameObject cam2;
     private Animator camAnim;
 
     public PlayerAtribsSO playerSO;
 
+    public CamAnimEnd camAlert;
+
+    public GameObject gameplayUI;
+
     private void Awake()
     {
         camAnim = cam.GetComponent<Animator>();
+        camAlert = cam.GetComponent<CamAnimEnd>();
+    }
+
+    private void Update()
+    {
+        if (camAlert.endAnim == true)
+            ChangeCamera();
     }
 
     public void PlayGame()
@@ -44,6 +56,13 @@ public class MenuController : MonoBehaviour
     public void ChangeFemale()
     {
         playerSO.Gender = "Female";
+    }
+
+    public void ChangeCamera()
+    {
+        cam.SetActive(false);
+        cam2.SetActive(true);
+        gameplayUI.SetActive(true);
     }
 
 }
