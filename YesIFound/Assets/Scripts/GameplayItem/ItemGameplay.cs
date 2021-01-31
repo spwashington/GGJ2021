8e1d57b995a83060de973d2771448541a54c74aa
Diego Jesus Almeida <diego.jacare@terra.com.br>
2021-01-30 23:26:23 -0300

    New Colors

--- YesIFound/Assets/Scripts/GameplayItem/ItemGameplay.cs
+++ YesIFound/Assets/Scripts/GameplayItem/ItemGameplay.cs
@@ -5,18 +5,35 @@ using UnityEngine;
 public class ItemGameplay : MonoBehaviour
 {
     [SerializeField] SpriteRenderer m_light, m_objSprite;
+    [SerializeField] Material MaterialSrc;
+
     private string m_ItemName, m_Color;
+    private Material ItemMaterial;
+    public string ItemColor { get => m_Color; }
+    public string ItemName { get => m_ItemName; }
+
+    Dictionary<string, Color> PossibleColors = new Dictionary<string, Color> { 
+        {"Red", Color.red },
+        {"Blue", Color.blue},
+        {"Yellow", Color.yellow},
+        {"Green", Color.green},
+        {"White", Color.white }
+    };
 
     private void Awake()
     {
         Unhighlight();
+        ItemMaterial = new Material(MaterialSrc);
     }
 
     public void Atributes(string _name, string _color, Sprite _sprite)
     {
         m_ItemName = _name;
         m_Color = _color;
+        ItemMaterial.color = PossibleColors[_color];
         m_objSprite.sprite = _sprite;
+        m_objSprite.material = ItemMaterial;
+
     }
 
     public void Highlight()
